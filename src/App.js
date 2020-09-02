a894624f846741a4e7fbd7bf946c8c0da2a05b8e
Kimsangwon <kimsw509@g2e.co.kr>
2020-09-02 09:00:49 +0900

    시작:tada:: 20200902

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
 
 function App() {
   return (
+
+    // BEM 벰이 뭔지 모르지만 공부 하기 said it's like a structure stuff 
     <div className="App">
-      <h1>에어 비앤비 클론 시작하기 </h1>
+      {/* {Home} */}
+      <Header />
+      <Home />
+          {/* Header */}
+
+          {/* {Banner} */}
+              {/* {Search} */}
+          {/* {Cards} */}
+
+          {/* {footer} */}
+
+      {/* {SearchPage} */}
+        {/* {} */}
     </div>
   );
 }
