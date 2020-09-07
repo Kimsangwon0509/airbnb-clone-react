529e8cf431c9cfbedc4965a5173c04f79d2c2a21
Kimsangwon <kimsw509@g2e.co.kr>
2020-09-07 09:31:23 +0900

    :fix:고침: DATE PICKER 해결

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './Search.css';
-// import { DateRangePicker } from "react-date-range";
-// import 'react-date-range/dist/styles.css'; // main style file
-// import 'react-date-range/dist/theme/default.css'; // theme css file
+ import { DateRangePicker } from "react-date-range";
+//  import 'react-date-range/dist/styles.css'; // main style file
+//  import 'react-date-range/dist/theme/default.css'; // theme css file
 
 // DATE PICKER COMPONENT
+// 이전에 date-fns 묘듈을 깔아야 함 .....module 불편하게 만듬...
 // npm i react-date-range 를 설치 해야 함
 function Search(props) {
     const[startDate,
