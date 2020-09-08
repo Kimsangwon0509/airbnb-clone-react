b2586f47ba4b2ba5baed9e3b87894289a2c8c140
Kimsangwon <kimsw509@g2e.co.kr>
2020-09-08 12:17:35 +0900

    :tada: date picker css completed

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './Search.css';
  import { DateRangePicker } from "react-date-range";
-//  import 'react-date-range/dist/styles.css'; // main style file
-//  import 'react-date-range/dist/theme/default.css'; // theme css file
+ import 'react-date-range/dist/styles.css'; // main style file
+ import 'react-date-range/dist/theme/default.css'; // theme css file
 
 // DATE PICKER COMPONENT
 // 이전에 date-fns 묘듈을 깔아야 함 .....module 불편하게 만듬...
