50f5e02892d4594a3de59ba2ec0b68e0f89355c0
Kimsangwon <kimsw509@g2e.co.kr>
2020-09-10 09:08:32 +0900

    :construction: 20200910

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(props) {
+    return (
+        <div className='footer'>
+            <p>© 2020 Airbnb, Inc. All rights reserved</p>
+            <p>개인정보 처리방침·이용약관·사이트맵·한국의 변경된 환불 정책 </p>
+        </div>
+    );
+}
+
+export default Footer;
